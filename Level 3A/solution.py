4934be56c819e880941ae778071cc2d0abe90c15
Ashutosh Srivastava <20560757+Ashutosh-Srivastava@users.noreply.github.com>
2020-06-25 23:23:04 +0530

    Update solution.py

--- Level 3A/solution.py
+++ Level 3A/solution.py
@@ -2,6 +2,7 @@
 '''
 Author: Ashutosh Srivastava
 Python3 solution
+You can also use the A* algorithm with little modification to solve the problem.
 '''
 
 from collections import deque
@@ -11,15 +12,15 @@ def solution(maze):
     q = deque([(0, 0, 1, True)])
     maze[0][0] = -1
     while (True):
-        x, y, step, bomb = q.popleft()
+        x, y, step, wall = q.popleft()
         if (x == h-1 and y == w-2 or x == h-2 and y == w-1): 
             return step+1
         for a, b in [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]:
-            if (not (0 <=a < h and 0 <=b < w) or maze[a][b] == -1 or maze[a][b] == -2 and not bomb or maze[a][b] == 1 and not bomb): 
+            if (not (0 <=a < h and 0 <=b < w) or maze[a][b] == -1 or maze[a][b] == -2 and not wall or maze[a][b] == 1 and not wall): 
               continue
             elif (maze[a][b] in [0, -2]):
-              q.append((a, b, step+1, bomb))
+              q.append((a, b, step+1, wall))
             elif (maze[a][b] == 1):
               q.append((a, b, step+1, False))
-            maze[a][b] = -1 if bomb else -2
+            maze[a][b] = -1 if wall else -2
     return maze[a][b]
